2997e23e9d0a692f1ed715bdbabfce5a439e25ed
LordNinth <rr621801@gmail.com>
2024-04-07 15:08:18 +0100

    authe

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require("express");
 const app = express();
 const User = require("./model/user"); //caps U since its a class/document
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/authe")
+  .then(() => {
+    console.log("connection open");
+  })
+  .catch((e) => {
+    console.log("error is:", e);
+  });
+
 
 //set view engine to ejs
 app.set("view engine", "ejs");
 app.set("views","views")
 
+app.use(express.urlencoded({extended:true}))
+
+
+app.get("/", (req, res) => {
+  res.send("homepage");
+});
 app.get("/register", (req, res) => {
   res.render("register");
 });
+
+app.post("/register",async(req,res)=>{
+    const {username, password } = req.body;
+    const hash= await bcrypt.hash(password,12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect("/")
+})
 app.get("/secret", (req, res) => {
   res.send("this is secret, can only be seen when logged in");
 });
