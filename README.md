5c825c05677958937d6710726de4e5c044544e19
LordNinth <rr621801@gmail.com>
2024-04-08 17:26:18 +0100

    authentication

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # auth
 
-npm i express ejs mongoose bcrypt
+npm i express ejs mongoose bcrypt express-session
+
+never store passwords in DB, only store the hashed passwords
+
+to remember who is logged in, and to see logged in views: We need sessions
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const User = require("./model/user"); //caps U since its a class/document
-const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/authe")
@@ -13,13 +14,12 @@ mongoose
     console.log("error is:", e);
   });
 
-
 //set view engine to ejs
 app.set("view engine", "ejs");
-app.set("views","views")
+app.set("views", "views");
 
-app.use(express.urlencoded({extended:true}))
-
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "thisissecret" }));
 
 app.get("/", (req, res) => {
   res.send("homepage");
@@ -28,18 +28,44 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register",async(req,res)=>{
-    const {username, password } = req.body;
-    const hash= await bcrypt.hash(password,12);
-    const user = new User({
-        username,
-        password: hash
-    })
-    await user.save();
-    res.redirect("/")
-})
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  //saving new user and hashed to DB
+  const user = new User({
+    username,
+    password: hash,
+  });
+  await user.save();
+  req.session.user_id = user._id;
+
+  res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username: username });
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (validPassword) {
+    req.session.user_id = user._id;
+    res.render("secret");
+  } else {
+    res.send("try again");
+  }
+});
+
+app.post("/logout", (req, res) => {
+  req.session.user_id = null; //set user id to null
+  res.redirect("/login");
+});
 app.get("/secret", (req, res) => {
-  res.send("this is secret, can only be seen when logged in");
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
+  res.render("secret");
 });
 
 app.listen(3000, (req, res) => {
